3e660f521d390dbb4f963d978881037b747de62b
himel999 <mahabubulalamofficial@gmail.com>
2021-02-06 00:46:28 +0600

    just include 0 in fibo_list

--- B.Creating_new_module/#A#fibonacci_module.py
+++ B.Creating_new_module/#A#fibonacci_module.py
@@ -1,18 +1,17 @@
-def find_fibo(x):
-    if x <= 2:
-        return 1
+def find_fib(n):
+    if n <= 2:
+        if n == 0:
+            return 0
+        else:
+            return 1
     fib_x, fib_next = 1, 1
 
     i = 3
-    while i <= x:
-        i = i + 1
-
-        fib_x, fib_next = fib_next, fib_x + fib_next
+    while i <= n:
+        i += 1
+        fib_x, fib_next = fib_next, fib_x+fib_next
     return fib_next
 
 
-for n in range(1, 10 + 1, 1):
-    if n < 10:
-        print(find_fibo(n), end=", ")
-    else:
-        print(find_fibo(n), end="")
+for x in range(0, 11):
+    print(find_fib(x), end=" ")
