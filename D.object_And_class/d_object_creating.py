7c900764c814c31dbe177385d31ad1422f26ff25
himel999 <mahabubulalamofficial@gmail.com>
2021-02-08 00:32:30 +0600

    object creating

---
+++ D.object_And_class/d_object_creating.py
@@ -0,0 +1,15 @@
+class Car:
+    name = ""
+    color = ""
+
+    def start(self):
+        print("Starting the engine")
+
+my_car = Car()
+my_car.name = "Allion"
+
+print(my_car.name)
+
+my_car.start()
+
+print(dir(Car))
