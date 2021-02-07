d7e665de07c80fbab2b3042f0b0a61d00d3deb1c
himel999 <mahabubulalamofficial@gmail.com>
2021-02-07 23:11:25 +0600

    Class creating

---
+++ D.object_And_class/b_class_creating.py
@@ -0,0 +1,12 @@
+class Car:
+    name = "Premio"
+    color = "white"
+
+    @staticmethod
+    def start():
+        print("Starting the engine")
+
+
+print("Name of the car :", Car.name)
+print("Color : ", Car.color)
+Car.start()
