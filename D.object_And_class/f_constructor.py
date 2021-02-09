fb86deb6ab524b72ad59de1d440f17ee739b3a93
himel999 <mahabubulalamofficial@gmail.com>
2021-02-09 13:41:31 +0600

    adding one more attribute

--- D.object_And_class/f_constructor.py
+++ D.object_And_class/f_constructor.py
@@ -1,21 +1,23 @@
 class Car:
 
-    def __init__(self, n, c):
+    def __init__(self, n, c, y):
         self.name = n
         self.color = c
+        self.year = y
 
         """here __init__ is a constructor"""
 
     def start(self):
         print("Car name is : ", self.name)
-        print("Car color is : ", self.color, end="\n\n")
+        print("Car color is : ", self.color)
+        print("Car color is : ", self.year, end="\n\n")
 
 
-print("First object:", end="\n")
-my_car = Car("Tesla", "green")
+print("First object:")
+my_car = Car("Tesla", "green", "1995")
 my_car.start()
 
 
 print("Second object:")
-my_car1 = Car("Corolla", "yellow")
+my_car1 = Car("Corolla", "yellow", "1995")
 my_car1.start()
