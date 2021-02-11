e2bcd867d3e6de2a22fea00bb216188d6a920e69
himel999 <mahabubulalamofficial@gmail.com>
2021-02-11 21:43:52 +0600

    browse html page from directory

--- E.request_module_and_file/d_web_page_save.py
+++ E.request_module_and_file/d_web_page_save.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import webbrowser as wb
 
 url = "https://www.premierfantasytools.com"
 
@@ -6,4 +8,11 @@ response = requests.get(url)
 
 f = open("fantasy.html", "w", encoding=response.encoding)
 f.write(response.text)
+
+file_path = os.path.relpath("fantasy.html")
+print(file_path)
+
+wb.open("file://" + file_path)
+
+
 
