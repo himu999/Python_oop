09925f96054fa871e198a34a7e20332344bdf06e
himel999 <mahabubulalamofficial@gmail.com>
2021-03-15 00:20:53 +0600

    search, \w, \w+, .+, .+h

--- I.regular_expression/c.w&w+.py
+++ I.regular_expression/c.w&w+.py
@@ -1,5 +1,23 @@
 import re
+
 s = "Bangladesh is our homeland"
 match = re.search("o\w\w", s)
 print(match.group())
+
+match = re.search("i\w\w", s)
+print(match)
+
+match = re.search("B\w+h", s)
+print(match.group())
+
+match = re.search("B.+h", s)
+print(match.group())
+
+match = re.search("B.+?h", s)
+print(match.group())
+
+
+
+
+
 
